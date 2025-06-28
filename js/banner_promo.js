7d80e19ee6d902c1d3d46115a7b16cb891e0c6e4
pavelmarkov2323 <rusgamer1997s@gmail.com>
2025-06-28 16:16:05 +0300

    Modal overlay update

--- js/banner_promo.js
+++ js/banner_promo.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {    
     document.querySelector('.close-banner-btn').addEventListener('click', () => {
-        const banner = document.querySelector('.advertisement-banner');
+        const banner = document.querySelector('.banner-promo');
         banner.classList.add('hidden');
         setTimeout(() => banner.style.display = 'none', 300);
     });
