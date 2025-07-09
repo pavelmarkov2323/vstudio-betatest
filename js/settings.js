b855f7425df63f3dc0db83cae73746b67b4348a2
pavelmarkov2323 <rusgamer1997s@gmail.com>
2025-07-09 10:28:21 +0300

    Update user profile + add card block info premium subscribe v7

--- js/settings.js
+++ js/settings.js
@@ -50,8 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Массив месяцев
     const months = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
+        '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', '10', '11', '12'
     ];
 
     // Массив стран (пример, нужно добавить больше)
