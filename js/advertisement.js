1c90e112253082b85078d1b552d7988da4c89d8a
pavelmarkov2323 <rusgamer1997s@gmail.com>
2025-07-25 15:47:09 +0300

    Update stable v2

--- js/advertisement.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {    
-    document.querySelector('.close-banner-btn').addEventListener('click', () => {
-        const banner = document.querySelector('.advertisement-banner');
-        banner.classList.add('hidden');
-        setTimeout(() => banner.style.display = 'none', 300);
-    });
-});
